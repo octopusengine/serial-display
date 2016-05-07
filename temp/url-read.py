1012d197c11a35feba74c2d9931b9a564d2e63fb
Honza S. Copak <bonum.comunae@volny.cz>
2016-05-07 14:53:07 +0200

    Added files via upload

---
+++ temp/url-read.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+import time               
+import urllib2             
+
+try:    
+    # response = urllib2.urlopen('http://tethys.cz/3dwarf?k=test1')
+    response = urllib2.urlopen('http://sentu.cz/get/datetime.php') 
+    novy_kod = response.read()
+    response.close() 
+except:
+    urlTxt= "url read err"
+
+print "date time: " , urlTxt 
+
