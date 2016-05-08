e0f0a4e43c60b8390c1f7823e9031e49146f7c93
Honza S. Copak <bonum.comunae@volny.cz>
2016-05-08 09:08:59 +0200

    Update url-read.py

--- temp/url-read.py
+++ temp/url-read.py
@@ -7,7 +7,7 @@ import urllib2
 try:    
     # response = urllib2.urlopen('http://tethys.cz/3dwarf?k=test1')
     response = urllib2.urlopen('http://sentu.cz/get/datetime.php') 
-    novy_kod = response.read()
+    urlTxt = response.read()
     response.close() 
 except:
     urlTxt= "url read err"
